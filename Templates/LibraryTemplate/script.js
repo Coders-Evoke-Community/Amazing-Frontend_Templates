bae9c91873ecfe9a67909daf38823cfb8b43809b
Yuzaki <74312170+Yuzaki76@users.noreply.github.com>
2021-10-29 11:15:13 +0530

    Delete script.js

--- Templates/LibraryTemplate/script.js
+++
@@ -1,7 +0,0 @@
-let url = window.location.href.substr(window.location.href.lastIndexOf("/") + 1)
-
-document.querySelectorAll(".nav-list a").forEach(element => {
-    if (element.attributes.href.value == url) {
-        element.classList.add("active");
-    }
-});
